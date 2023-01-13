468f78f97beddfe884d2cf5394eb86034e30d9ca
Latibro <mail@latibro.com>
2023-01-13 15:41:33 +0100

    First-take on villager only trading what they have in inventory

---
+++ common/src/main/java/net/examplemod/inventoryinspector/VillagerInventoryInspectorItem.java
@@ -0,0 +1,36 @@
+package net.examplemod.inventoryinspector;
+
+import dev.architectury.registry.menu.MenuRegistry;
+import net.examplemod.ExampleMod;
+import net.examplemod.inventoryinspector.villagerinventory.VillagerInventoryMenuProvider;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class VillagerInventoryInspectorItem extends Item {
+    public VillagerInventoryInspectorItem() {
+        super(new Properties().tab(ExampleMod.EXAMPLE_TAB));
+    }
+
+    @Override
+    public InteractionResult interactLivingEntity(ItemStack itemStack, Player player, LivingEntity entity, InteractionHand hand) {
+        System.out.println("XXX: Interact entity");
+
+        if (entity instanceof Villager villager) {
+            System.out.println("XXX: Interact villager");
+            if (player instanceof ServerPlayer serverPlayer) {
+                MenuRegistry.openMenu(serverPlayer, new VillagerInventoryMenuProvider(villager));
+                //player.openMenu(new VillagerInventoryMenuProvider(villager));
+            }
+            return InteractionResult.SUCCESS;
+        }
+
+        return InteractionResult.FAIL;
+    }
+
+}
---
+++ common/src/main/java/net/examplemod/inventoryinspector/villagerinventory/InventoryMerchantOffer.java
@@ -0,0 +1,166 @@
+package net.examplemod.inventoryinspector.villagerinventory;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.trading.MerchantOffer;
+
+public class InventoryMerchantOffer extends MerchantOffer {
+
+    private final MerchantOffer originalOffer;
+    private final SimpleContainer merchantInventory;
+
+    private int uses;
+    private int demand;
+
+
+    public InventoryMerchantOffer(MerchantOffer originalOffer, SimpleContainer merchantInventory) {
+        super(ItemStack.EMPTY, ItemStack.EMPTY, 0, 0, 0);
+        this.originalOffer = originalOffer;
+        this.merchantInventory = merchantInventory;
+    }
+
+    @Override
+    public ItemStack getBaseCostA() {
+        return originalOffer.getBaseCostA();
+    }
+
+    @Override
+    public ItemStack getCostA() {
+        return originalOffer.getCostA();
+    }
+
+    @Override
+    public ItemStack getCostB() {
+        return originalOffer.getCostB();
+    }
+
+    @Override
+    public ItemStack getResult() {
+        return originalOffer.getResult();
+    }
+
+    @Override
+    public void updateDemand() {
+        demand = demand + uses - (getMaxUses() - uses);
+    }
+
+    @Override
+    public ItemStack assemble() {
+        return originalOffer.assemble();
+        /*
+        for (int i = 0; i < inventory.getContainerSize(); i++) {
+            ItemStack itemStack = inventory.getItem(i);
+            if (itemStack.sameItem(getResult())) {
+                return itemStack.split(1);
+            }
+        }
+
+        return ItemStack.EMPTY;
+         */
+    }
+
+    @Override
+    public int getUses() {
+        return uses;
+    }
+
+    @Override
+    public void resetUses() {
+        uses = 0;
+    }
+
+    @Override
+    public int getMaxUses() {
+        if (offersTrade()) {
+            return merchantInventory.countItem(getResult().getItem()) / getResult().getCount();
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public void increaseUses() {
+        uses++;
+    }
+
+    @Override
+    public int getDemand() {
+        return demand;
+    }
+
+    @Override
+    public void addToSpecialPriceDiff(int i) {
+        originalOffer.addToSpecialPriceDiff(i);
+    }
+
+    @Override
+    public void resetSpecialPriceDiff() {
+        originalOffer.resetSpecialPriceDiff();
+    }
+
+    @Override
+    public int getSpecialPriceDiff() {
+        return originalOffer.getSpecialPriceDiff();
+    }
+
+    @Override
+    public void setSpecialPriceDiff(int i) {
+        originalOffer.setSpecialPriceDiff(i);
+    }
+
+    @Override
+    public float getPriceMultiplier() {
+        return originalOffer.getPriceMultiplier();
+    }
+
+    @Override
+    public int getXp() {
+        return originalOffer.getXp();
+    }
+
+    private boolean offersTrade() {
+        return merchantInventory.countItem(getResult().getItem()) >= getResult().getCount()
+                && merchantInventory.canAddItem(getCostA())
+                && (getCostB().isEmpty() || merchantInventory.canAddItem(getCostB()));
+    }
+
+    @Override
+    public boolean isOutOfStock() {
+        return !offersTrade();
+    }
+
+    @Override
+    public void setToOutOfStock() {
+        uses = getMaxUses();
+    }
+
+    @Override
+    public boolean needsRestock() {
+        return uses > 0;
+    }
+
+    @Override
+    public boolean shouldRewardExp() {
+        return originalOffer.shouldRewardExp();
+    }
+
+    @Override
+    public CompoundTag createTag() {
+        CompoundTag compoundTag = originalOffer.createTag();
+        compoundTag.putInt("uses", uses);
+        compoundTag.putInt("maxUses", getMaxUses());
+        compoundTag.putInt("demand", demand);
+        return compoundTag;
+    }
+
+    @Override
+    public boolean satisfiedBy(ItemStack costAItemStack, ItemStack costBItemStack) {
+        return originalOffer.satisfiedBy(costAItemStack, costBItemStack);
+    }
+
+    @Override
+    public boolean take(ItemStack costAItemStack, ItemStack costBItemStack) {
+        return originalOffer.take(costAItemStack, costBItemStack);
+    }
+}
