9a9d2092f3c19a9a721730db477631a711117d49
Latibro <mail@latibro.com>
2023-01-29 16:04:50 +0100

    Expanded villagers inventory to 27 slots (from build-in 8 slots)

--- common/src/main/java/latibro/minecraft/enhancedvillagers/mixin/AbstractVillagerMixin.java
+++ common/src/main/java/latibro/minecraft/enhancedvillagers/mixin/AbstractVillagerMixin.java
@@ -1,6 +1,9 @@
 package latibro.minecraft.enhancedvillagers.mixin;
 
+import latibro.minecraft.enhancedvillagers.inventory.VillagerInventory;
 import latibro.minecraft.enhancedvillagers.trade.TradeOffersManager;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.npc.InventoryCarrier;
 import net.minecraft.world.entity.npc.VillagerTrades;
@@ -8,14 +11,34 @@ import net.minecraft.world.inventory.MerchantMenu;
 import net.minecraft.world.item.trading.Merchant;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.item.trading.MerchantOffers;
+import net.minecraft.world.level.Level;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractVillager.class)
 public abstract class AbstractVillagerMixin implements InventoryCarrier, Merchant {
+
+    private final VillagerInventory villagerInventory = new VillagerInventory((AbstractVillager) (Object) this);
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void mixinConstructor(EntityType entityType, Level level, CallbackInfo ci) {
+    }
+
+    @Override
+    public SimpleContainer getInventory() {
+        return this.villagerInventory;
+    }
+
+    @Redirect(method = "*", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/npc/AbstractVillager;inventory:Lnet/minecraft/world/SimpleContainer;", opcode = Opcodes.GETFIELD))
+    private SimpleContainer mixinFieldGetInventory(AbstractVillager villager) {
+        System.out.println("XXX: Get inventory field redirect " + this);
+        return villager.getInventory();
+    }
 
     @Override
     //@Overwrite()
@@ -31,13 +54,11 @@ public abstract class AbstractVillagerMixin implements InventoryCarrier, Merchan
 
     @Inject(method = "notifyTrade", at = @At("TAIL"))
     private void mixinNotifyTrade(MerchantOffer offer, CallbackInfo callbackInfo) {
-        System.out.println("XXX: Notify trade " + this);
+        System.out.println("XXX: Notify trade " + this + this.getInventory());
 
         getInventory().removeItemType(offer.getResult().getItem(), offer.getResult().getCount());
         getInventory().addItem(offer.getCostA());
-        if (!offer.getCostA().isEmpty()) {
-            getInventory().addItem(offer.getCostB());
-        }
+        getInventory().addItem(offer.getCostB());
 
         MerchantMenu merchantMenu = (MerchantMenu) getTradingPlayer().containerMenu;
         MerchantOffers merchantOffers = getOffers();
