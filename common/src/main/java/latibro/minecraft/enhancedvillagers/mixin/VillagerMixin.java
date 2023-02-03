0d29cc2b5da253a223edb91152b1f660fdd4b450
Latibro <mail@latibro.com>
2023-02-03 13:02:14 +0100

    Replaced mixin overrides with overwrites

--- common/src/main/java/latibro/minecraft/enhancedvillagers/mixin/VillagerMixin.java
+++ common/src/main/java/latibro/minecraft/enhancedvillagers/mixin/VillagerMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.trading.MerchantOffers;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -19,18 +20,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Villager.class)
 public abstract class VillagerMixin extends AbstractVillagerMixin implements VillagerDataHolder {
 
+    /**
+     * @author Latibro
+     * @reason Make suse direct field access to "offers" is redirected to "getOffers()"
+     */
     @Redirect(method = "*", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/npc/Villager;offers:Lnet/minecraft/world/item/trading/MerchantOffers;", opcode = Opcodes.GETFIELD))
     private MerchantOffers mixinFieldGetOffers(Villager villager) {
         System.out.println("XXX: Get offer field redirect " + this);
         return villager.getOffers();
     }
 
+    /**
+     * @author Latibro
+     * @reason Make suse direct field access to "offers" is redirected to "setOffers()"
+     */
     @Redirect(method = "*", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/npc/Villager;offers:Lnet/minecraft/world/item/trading/MerchantOffers;", opcode = Opcodes.PUTFIELD))
     private void mixinFieldSetOffers(Villager villager, MerchantOffers offers) {
         System.out.println("XXX: Set offer field redirect " + this);
         villager.setOffers(offers);
     }
 
+    /**
+     * @author Latibro
+     * @reason Replacing trade system
+     */
     @Inject(method = "setOffers", at = @At("HEAD"), cancellable = true)
     public void mixinSetOffers(MerchantOffers merchantOffers, CallbackInfo ci) {
         System.out.println("XXX: Cancel set offers " + this);
@@ -43,6 +56,10 @@ public abstract class VillagerMixin extends AbstractVillagerMixin implements Vil
         return getVillagerData().getLevel();
     }
 
+    /**
+     * @author Latibro
+     * @reason Cancelling stopTrading() if villager is unemployed, as we want unemployed villagers to be able to trade
+     */
     @Redirect(method = "customServerAiStep", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/npc/Villager;stopTrading()V"))
     private void mixinCancelStopTradingUnemployedVillager(Villager villager) {
         //System.out.println("XXX: Cancel stop trading for unemployed villager " + this);
@@ -50,13 +67,21 @@ public abstract class VillagerMixin extends AbstractVillagerMixin implements Vil
         // Allowing all villagers to trade
     }
 
-    @Override
-    protected void updateTrades() {
+    /**
+     * @author Latibro
+     * @reason Replacing trade system
+     */
+    @Overwrite
+    public void updateTrades() {
         System.out.println("XXX: Update trades " + this);
         // Cancel original code
         // Do nothing
     }
 
+    /**
+     * @author Latibro
+     * @reason Cancel start trade if used item is inventory inspector
+     */
     @Inject(method = "mobInteract", at = @At("HEAD"), cancellable = true)
     protected void mixinMobInteract(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
         ItemStack itemstack = player.getItemInHand(hand);
