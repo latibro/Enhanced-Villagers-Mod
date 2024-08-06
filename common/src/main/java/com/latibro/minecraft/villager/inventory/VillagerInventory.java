9a0993347730520249688a5b4d1b3395ac2785e5
Latibro <mail@latibro.com>
2024-08-06 11:23:50 +0200

    Change villager inventory size from 27 to 36, to match same size as player (3x9 + 9 hotbar)

--- common/src/main/java/com/latibro/minecraft/villager/inventory/VillagerInventory.java
+++ common/src/main/java/com/latibro/minecraft/villager/inventory/VillagerInventory.java
@@ -5,7 +5,7 @@ import net.minecraft.world.entity.npc.AbstractVillager;
 
 public class VillagerInventory extends SimpleContainer {
 
-    private static final int INVENTORY_SIZE = 27; // 3 x 9
+    private static final int INVENTORY_SIZE = 36; // 3 x 9
     private final AbstractVillager villager;
 
     public VillagerInventory(AbstractVillager villager) {
